27d0a3d63b7adf2787c00f2b9833c288f464a7c3
Curtis Kauer <kauercurtis@gmail.com>
2023-10-07 20:25:15 -0700

    initial refactoring

---
+++ main.rb
@@ -0,0 +1,26 @@
+class Bicycle
+    # ...
+  end
+  
+  class RedBicycle < Bicycle
+  
+    WEIGHT_CHANGE_AMOUNT = 10
+  
+    def initialize(height, weight, color)
+      @height = height
+      @weight = weight
+      @color = color
+    end
+  
+    def getColor
+      @color
+    end
+  
+    def getHeight
+      @height
+    end
+  
+    def decreaseByAmount
+      @weight -= WEIGHT_CHANGE_AMOUNT
+    end
+  end
